60e2c4493d26dd30ff0f7de99e1445d39d8fdd16
MarkazGasimov <mqasimov5@std.beu.edu.az>
2022-11-19 09:12:56 +0000

    continue

--- app.js
+++ app.js
@@ -7,49 +7,35 @@ let youchoose = document.querySelector("#youchoose")
 let compchoose = document.querySelector("#compchoose")
 let btn = document.querySelector(".btn")
 
-rock.addEventListener('click', function (name) {
+rock.addEventListener('click', function () {
     youchoose.innerHTML = 'Rock'
     var things = ['Rock', 'Paper', 'Scissor'];
     var thing = things[Math.floor(Math.random() * things.length)]
     compchoose.innerHTML = thing
 })
-paper.addEventListener('click', function (name) {
-    youchoose.innerHTML = 'Paper'
-    var things = ['Rock', 'Paper', 'Scissor'];
-    var thing = things[Math.floor(Math.random() * things.length)]
-    compchoose.innerHTML = thing
-})
-scissors.addEventListener('click', function (name) {
-    youchoose.innerHTML = 'Scissors'
-    var things = ['Rock', 'Paper', 'Scissor'];
-    var thing = things[Math.floor(Math.random() * things.length)]
-    compchoose.innerHTML = thing
-})
-
 btn.addEventListener('click', function (name) {
-    if (youchoose == rock & compchoose == paper) {
-        console.log("You lose!!");
-    }else if(youchoose == rock & compchoose == scissors){
-        console.log("You win!!");
-    }else{
+    if (youchoose == rock & compchoose == rock) {
         console.log("Equal");
     }
 })
 
-// btn.addEventListener('click', function (name) {
-//     if (youchoose == paper & compchoose == scissors) {
-//         console.log("You lose!!");
-//     }else if(youchoose == paper & compchoose == paper){
-//         console.log("Equal");
-//     }else{
-//         console.log("You win!!");
-//     }
+
+
+
+
+
+// paper.addEventListener('click', function () {
+//     youchoose.innerHTML = 'Paper'
+//     var things = ['Rock', 'Paper', 'Scissor'];
+//     var thing = things[Math.floor(Math.random() * things.length)]
+//     compchoose.innerHTML = thing
 // })
 
-// btn.addEventListener('click', function (name) {
-//     if (youchoose == rock & compchoose == scissors) {
-//         console.log("You win!!");
-//     }else if(youchoose == rock & compchoose == paper){
-//         console.log("you lose!!");
-//     }
+// scissors.addEventListener('click', function () {
+//     youchoose.innerHTML = 'Scissors'
+//     var things = ['Rock', 'Paper', 'Scissor'];
+//     var thing = things[Math.floor(Math.random() * things.length)]
+//     compchoose.innerHTML = thing
 // })
+
+
